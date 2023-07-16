885c00b025fbd0a3a5b0c480f7ac2abd847239ca
Anton Shchukin <a.a.shchukin@gmail.com>
2023-07-16 09:07:04 +0700

    Пробуем вызвать компонент Main из App

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
-import Main from './pages/main/main';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
@@ -10,6 +9,5 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <App />
-    <Main />
   </React.StrictMode>,
 );
