3c9c63ad571bf9ccfb48cd33bae18c585e8c5dfa
Anton Shchukin <a.a.shchukin@gmail.com>
2023-07-16 09:04:55 +0700

    Черновик компонента-страницы Main

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
+import Main from './pages/main/main';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
@@ -9,5 +10,6 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <App />
+    <Main />
   </React.StrictMode>,
 );
