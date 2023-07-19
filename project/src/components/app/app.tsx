9bf8d757f68a2abbb7cba091426834840cc6ca9e
Anton Shchukin <a.a.shchukin@gmail.com>
2023-07-19 10:59:38 +0700

    basic pages

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,4 +1,9 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+
 import Main from './../../pages/main/main';
+import Login from './../../pages/login/login';
+import Favorites from './../../pages/favorites/favorites';
+import Property from '../../pages/property/property';
 
 type App = {
   offersAmountOnMainPage: number;
@@ -6,7 +11,14 @@ type App = {
 
 function App({offersAmountOnMainPage}: App): JSX.Element {
   return (
-    <Main offersAmount={offersAmountOnMainPage}/>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main offersAmount={offersAmountOnMainPage}/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/favorites" element={<Favorites/>}/>
+        <Route path="/offer/:id" element={<Property/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
