552b78f67dfab6cb10a521bbb5c14d519682d980
Anton Shchukin <a.a.shchukin@gmail.com>
2023-07-19 11:31:00 +0700

    back link on error page

--- project/src/pages/error/error.tsx
+++ project/src/pages/error/error.tsx
@@ -1,3 +1,5 @@
+import {Link} from 'react-router-dom';
+
 function Error(): JSX.Element {
   return (
     <div className="page page--error">
@@ -29,15 +31,18 @@ function Error(): JSX.Element {
           </div>
         </div>
       </header>
-
       <main className="page__main page__main--error">
         <div className="page__error-container container">
           <section className="error">
             <div className="error__wrapper">
               <h1 className="error__title">404</h1>
-              <div className="error__message">Страница не найдена</div>
+              <div className="error__message">Page not found</div>
+              <Link className="error__back" to="/">
+                <span className="error__arrow">←</span>
+                &nbsp;
+                Main page
+              </Link>
             </div>
-
           </section>
         </div>
       </main>
