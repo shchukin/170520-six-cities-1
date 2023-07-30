913c653a766508defac806719414374ddfbd90a6
Anton Shchukin <a.a.shchukin@gmail.com>
2023-07-30 11:26:11 +0700

    near places

---
+++ project/src/components/near-places/near-places.tsx
@@ -0,0 +1,26 @@
+import {offersData} from '../../mocks/offersData';
+import PlaceCard from '../place-card/place-card';
+
+type NearPlacesProps = {
+  placeId: string;
+}
+function NearPlaces():JSX.Element {
+  return (
+    <div className="container">
+      <section className="near-places">
+        <h2 className="near-places__title">
+          Other places in the neighbourhood
+        </h2>
+        <div className="near-places__list">
+          {
+            offersData.slice(0, 3).map((element, index) => <div className={'near-places__card'} key={element.id}><PlaceCard data={element}/></div>)
+          }
+        </div>
+      </section>
+    </div>
+  )
+}
+
+
+export default NearPlaces();
+
