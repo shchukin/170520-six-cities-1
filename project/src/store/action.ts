2c41d7ece9c2b2cf8ed1c9b3f263b449b76d85ad
Anton Shchukin <a.a.shchukin@gmail.com>
2023-11-06 18:10:17 +0700

    comment

--- project/src/store/action.ts
+++ project/src/store/action.ts
@@ -2,7 +2,7 @@ import {createAction} from '@reduxjs/toolkit';
 
 export const changeCity = createAction('CHANGE_CITY', (city) => {
   return {
-    payload: {
+    payload: { /* Payload нужен, чтобы передать переменную */
       city
     },
   };
