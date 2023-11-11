6f02c107063ff8630759191e29b1b66e6993672c
Anton Shchukin <a.a.shchukin@gmail.com>
2023-11-11 12:39:59 +0700

    sorting component

--- project/src/pages/main/main.tsx
+++ project/src/pages/main/main.tsx
@@ -4,6 +4,7 @@ import Footer from '../../components/footer/footer';
 import Map from '../../components/map/map';
 import {settings} from '../../settings';
 import Locations from '../../components/locations/locations';
+import PlacesSorting from '../../components/places-sorting/places-sorting';
 import {useAppSelector} from '../../index';
 
 
@@ -25,21 +26,7 @@ function Main(): JSX.Element {
             <section className="cities__places places">
               <h2 className="visually-hidden">Places</h2>
               <b className="places__found">312 places to stay in {currentCity}</b>
-              <form className="places__sorting" action="#" method="get">
-                <span className="places__sorting-caption">Sort by</span>
-                <span className="places__sorting-type" tabIndex={0}>
-                  Popular
-                  <svg className="places__sorting-arrow" width="7" height="4">
-                    <use xlinkHref="#icon-arrow-select"></use>
-                  </svg>
-                </span>
-                <ul className="places__options places__options--custom places__options--opened">
-                  <li className="places__option places__option--active" tabIndex={0}>Popular</li>
-                  <li className="places__option" tabIndex={0}>Price: low to high</li>
-                  <li className="places__option" tabIndex={0}>Price: high to low</li>
-                  <li className="places__option" tabIndex={0}>Top rated first</li>
-                </ul>
-              </form>
+              <PlacesSorting />
               <div className="cities__places-list places__list tabs__content">{
                 offersData.slice(0, settings.OffersAmountOnMainPage).map((element) => <div className={'cities__card'} key={element.id}><PlaceCard data={element}/></div>)
               }
