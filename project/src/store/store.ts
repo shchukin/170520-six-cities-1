0ca12314490fb39e7edc7fa429eb6bc6d2bbde56
Anton Shchukin <a.a.shchukin@gmail.com>
2023-12-16 07:43:48 +0700

    redux-thunk

--- project/src/store/store.ts
+++ project/src/store/store.ts
@@ -1,7 +1,17 @@
 import {configureStore} from '@reduxjs/toolkit';
 import {cityReducer} from './reducer';
+import {createAPI} from '../services/api';
+
+export const api = createAPI();
+
 
 export const store = configureStore({
-  reducer: cityReducer
+  reducer: cityReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      thunk: {
+        extraArgument: api,
+      },
+    }),
 });
 
